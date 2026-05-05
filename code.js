0dd7d2ca0a1b050cc2984382d68fdd5ab6343766
Fiona <fionambuguanjeri@gmail.com>
2026-05-06 00:02:15 +0300

    adding a new task js

--- code.js
+++ code.js
@@ -1,7 +1,21 @@
+//adding a new task
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 const form = document.getElementById("taskForm");
 
 if (form) {
-form.addEventListener("submit", function(e) {
-e.preventDefault();
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+
+    let task = { title: title, description: description, completed: false };
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    window.location.href = "about.html";
+  });
+}
+
+//displaying
